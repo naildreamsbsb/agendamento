a32c2e22514f519381952787445fbb8b5fc59102
Agendamento <naildreams.bsb@gmail.com>
2026-07-09 14:32:55 -0300

    atualiza url da api com resumo de caixa

--- config/config.js
+++ config/config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycby-XS-nAciwROLKJnvxBEV_a_XDboFGXPFu3rbKF8JC16hZc9xmCVhxCAiHpw-wLZ5bvw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwgXzPC1hbu3e4bm-2i5DXZj13JVPJb30OYBcOrWBjXpqqLSOIJcOi4B70T9EUkjnMqCA/exec";
